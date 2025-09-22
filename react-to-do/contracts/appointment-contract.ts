2108b9e6c9afd8ace2a79909b9e76c3a222c9ada
unknown <priyanshusolon2001@gmail.com>
2025-09-22 16:26:03 +0530

    ok

---
+++ react-to-do/contracts/appointment-contract.ts
@@ -0,0 +1,8 @@
+export interface AppointmentContract
+               {
+                 appointment_id: number;
+                 title:string;
+                 description:string;
+                 date: date;
+                 user_id: string;
+              }
---
+++ react-to-do/contracts/user-contract.ts
@@ -0,0 +1,7 @@
+export interface UserContract
+               {
+                   user_id: string;
+                   user_name:string;
+                   password:string;
+                   email:string;
+               }
